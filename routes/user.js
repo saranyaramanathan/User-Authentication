748f32b2ec756761a5e8d5fc96130c11fdfffc7a
Saranya <saranya@credentech.net>
2025-07-25 14:42:51 +0530

    route changes

--- routes/user.js
+++ routes/user.js
@@ -3,7 +3,7 @@ const { userSignUp, userLogin, getUser } = require("../controller/users");
 const {authenticate}=require("../middleware/auth")
 const router=express.Router();
 
-router.post("/",userSignUp);    //register a user
+router.post("/register",userSignUp);    //register a user
 router.post("/login",userLogin);     //user login 
 router.get("/:id",authenticate,getUser);        //get user
 module.exports=router;
